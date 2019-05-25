7f8b3172bb182083135c3507d4c16e1fb8f3625c
eOguzhan <e.oguzhan.yildirim@gmail.com>
2019-05-25 18:27:06 +0300

    Restoran listeleme sayfasina buton eklendi.

--- background.js
+++ background.js
@@ -1,1 +1,31 @@
-alert("Hello World");
+// Yemek begenim htmli cikartacak.
+function kararButonuEvent() {
+	// FIXME: Simlilik alert basiyor.
+	alert("Tinderla beni");
+}
+
+// Sayfanin gercekten restoran listeleme sayfasi mi oldugunu kontrol eder.
+function restoranListelemeEkraniMi() {
+	// FIXME: Simdilik true donuyor.
+	return true;
+}
+
+// Sayfa yuklendikten sonra cagirilir.
+function onWindowLoad() {
+	// Restoran listeleme ekrani degilse pas gecilir.
+	if (!restoranListelemeEkraniMi()) {
+		return;
+	}
+	
+	var kararButonu = document.createElement("button");
+	kararButonu.innerHTML = "Karar Veremiyorum";
+	kararButonu.className = "ys-btn ys-btn-info ys-btn-xs";
+	kararButonu.id = "kararButonu";
+	kararButonu.addEventListener("click", kararButonuEvent);
+	kararButonu.style = "padding-top:4px; padding-bottom:4px;";
+	
+	var butonListesi = document.body.getElementsByClassName("ys-reslist-sort init")[0];
+	butonListesi.append(kararButonu); 	// Karar Veremiyorum butonu sayfaya eklenir.
+}
+
+window.onload = onWindowLoad;
