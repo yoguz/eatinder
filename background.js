0d155866a11be0c6c9cc6fe7f9cea8c0b5cad656
eOguzhan <e.oguzhan.yildirim@gmail.com>
2019-07-06 15:03:00 +0300

    some functions added

--- background.js
+++ background.js
@@ -1,15 +1,72 @@
+var cuisinePictureMap = {
+	"Burger": "burger.jpg",
+	"Döner": "burger.jpg",
+	"Ev Yemekleri": "burger.jpg",
+	"Fast Food & Sandwich": "burger.jpg",
+	"Kahvaltı": "burger.jpg",
+	"Kebap & Türk Mutfağı": "burger.jpg",
+	"Kumpir": "burger.jpg",
+	"Köfte": "burger.jpg",
+	"Pasta & Tatlı": "burger.jpg",
+	"Pide": "burger.jpg",
+	"Pizza & İtalyan": "burger.jpg",
+	"Tantuni": "burger.jpg",
+	"Tavuk": "burger.jpg",
+	"Çiğ Köfte": "burger.jpg"
+}
+
+var swipeCuisines = []
+var likedCuisines = []
+var currentCuisine;
+
+// Selects 5 randomly cuisines and fills them into swipeCuisines list
+function fillSwipeCuisineList(cuisineCountArray) {
+	var addCount = 0;
+	var i = 0;
+	while (addCount < 5 && i < cuisineCountArray.length ) {
+		if (cuisinePictureMap[cuisineCountArray[i].cuisine] !== undefined) {
+			swipeCuisines.push(cuisineCountArray[i].cuisine);
+			addCount++;
+		}
+		i++;
+	}
+}
+
+// shows next food picture if any left, or shows match picture
+function showNextUnswipedCuisine(){
+	if (swipeCuisines.length > 0) {		// Show next food picture
+		var nextCuisine = swipeCuisines.pop();
+		console.log(nextCuisine);
+		var cuisineJpg = cuisinePictureMap[nextCuisine];
+		var jpegImgElem = document.getElementById("eatinder-image-img");
+		jpegImgElem.src = "chrome-extension://hfchedmaclelneclbdllnchgfklddpcj" + "/images/" + cuisineJpg;
+	} else {	// Show match picture
+	
+	}
+}
+
+function eatinderLiked() {
+	likedCuisines.push(currentCuisine);
+	showNextUnswipedCuisine();
+}
+
 // It will pop-up the like-dislike page
 function cantDecideButtonEvent() {
-	//getAllCuisinesWithCounts();
-	var s = document.createElement('script');
-	var decisionDiv = document.createElement('div');
-	decisionDiv.id = 'decisionDiv';
-	document.body.append(decisionDiv);
+	var cuisineCountArray = getAllCuisinesWithCounts();
+	fillSwipeCuisineList(cuisineCountArray);
+	/*var s = document.createElement('script');
 	s.src = chrome.runtime.getURL('decision-page-loader.js');
 	s.onload = function() {
 		this.parentNode.removeChild(this);
 	};
 	(document.head||document.documentElement).appendChild(s);
+	*/
+	var decisionDiv = document.createElement('div');
+	decisionDiv.id = 'decisionDiv';
+	document.body.append(decisionDiv);
+	$('#decisionDiv').load('chrome-extension://hfchedmaclelneclbdllnchgfklddpcj/decision.html', function(){
+		showNextUnswipedCuisine();
+	});
 }
 
 // Returns {cuisine, count} object list
@@ -29,12 +86,10 @@ function getAllCuisinesWithCounts() {
 		}
 	}
 	var cuisineKeyValueMap = [];
-	console.log(cuisineMap);
 	for (var cuisine in cuisineMap) {
 		cuisineKeyValueMap.push({"cuisine": cuisine, "count": cuisineMap[cuisine]});
 	}
 	cuisineKeyValueMap.sort((a, b) => (a.count > b.count) ? -1 : 1);
-	
 	return cuisineKeyValueMap;
 }
 
