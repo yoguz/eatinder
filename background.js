a9c4108f179b21be6bbd440552c7f99564dfebaa
eOguzhan <e.oguzhan.yildirim@gmail.com>
2019-07-10 21:54:15 +0300

    background and manifest updated

--- background.js
+++ background.js
@@ -41,7 +41,7 @@ function fillSwipeCuisineList(cuisineCountArray) {
 	}
 }
 
-function matchScreenAndResult() {
+function showResults() {
 	// Match screen comes here
 	
 	var allRestaurantList = document.getElementsByClassName("ys-reslist")[0].getElementsByClassName("ys-item");
@@ -88,6 +88,8 @@ function matchScreenAndResult() {
 	}
 	
 	console.log(finalRestaurantList);
+	var objTooltip = JSON.parse(finalRestaurantList[0].childNodes[1].childNodes[3].childNodes[1].dataset.tooltip);
+	console.log(objTooltip["ImageFullPath"]);
 }
 
 // shows next food picture if any left, or shows match picture
@@ -99,7 +101,8 @@ function showNextUnswipedCuisine(){
 		jpegImgElem.src = chrome.extension.getURL("images/" + cuisineJpg);
 		eatinderCurrentCuisine = nextCuisine;
 	} else {	// Show match picture
-		matchScreenAndResult();
+		showMatchScreen();
+		showResults();
 	}
 }
 
@@ -111,7 +114,16 @@ function cantDecideButtonEvent() {
 	var decisionDiv = document.createElement('div');
 	decisionDiv.id = 'eatinder-decision-div';
 	decisionDiv.tabindex = "-1";
-	decisionDiv.style = "display: block; z-index: 1; visibility: visible; top: 0px; left: 0px; position: fixed; width: 100%; height: 100%; background-color:rgba(185, 176, 176, 0.65);";
+	decisionDiv.style = "display: block; z-index: 1; visibility: visible; top: 0px; left: 0px; position: fixed; width: 100%; height: 100%;";
+	
+	var decisionBackgroundImgDiv = document.createElement("div");
+	decisionBackgroundImgDiv.id = 'eatinder-decision-background-img-div';
+	decisionBackgroundImgDiv.style= "top: 9%; left:28%; width: 44%; height: 90%; position:fixed; background-color:rgba(255, 255, 255, 1); border-radius:4px;";
+	
+	var decisionBackgroundImgImg = document.createElement("img");
+	decisionBackgroundImgImg.id = 'eatinder-decision-background-img-img';
+	decisionBackgroundImgImg.src =  chrome.extension.getURL("images/Pattern_Food_1.jpg");
+	decisionBackgroundImgImg.style= "top: 10%; left:29%; width: 42%; height: 90%; position:fixed; opacity: 0.8";
 	
 	var decisionImgDiv = document.createElement('div');
 	decisionImgDiv.style = "top: 15%; left:30%; width: 40%; height: 60%; position:fixed";
@@ -123,137 +135,24 @@ function cantDecideButtonEvent() {
 	var decisionDislikeDiv = document.createElement('div');
 	decisionDislikeDiv.style = "top: 77%; left:30%; width: 15%; height: 10%; position:fixed";
 	
-	var decisionDislikeButton = document.createElement("button");
+	var decisionDislikeButton = document.createElement("div");
 	decisionDislikeButton.id = "eatinder-dislike-btn";
-	$(document).ready(function(){
-		$("#eatinder-dislike-btn").css({
-			borderRadius:"32px",
-			border:"3px solid #d02718",
-			display:"inline-block",
-			cursor:"pointer",
-			color:"#ffffff",
-			fontFamily:"Georgia",
-			fontSize:"16px",
-			fontWeight:"bold",
-			padding:"11px 37px",
-			textDecoration:"none",
-			textShadow:"0px -2px 0px #820808",
-			width:"200px",
-			backgroundImage:"-moz-box-shadow:inset 0px 1px 0px 0px #bd1a0b",
-			backgroundImage:"-webkit-box-shadow:inset 0px 1px 0px 0px #bd1a0b",
-			boxShadow:"inset 0px 1px 0px 0px #bd1a0b",
-			backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f24537), color-stop(1, #c62d1f))",
-			backgroundImage:"-moz-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-webkit-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-o-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-ms-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"linear-gradient(to bottom, #f24537 5%, #c62d1f 100%)",
-			filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#f24537', endColorstr='#c62d1f',GradientType=0)",
-			backgroundColor:"#f24537",
-			backgroundImage:"-moz-border-radius:32px",
-			backgroundImage:"-webkit-border-radius:32px"
-		});
-		$("#eatinder-dislike-btn").hover(function(){
-			$(this).css({
-				backgroundColor:"#c62d1f",
-				backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #c62d1f), color-stop(1, #f24537))",
-				backgroundImage:"-moz-linear-gradient(top, #c62d1f 5%, #f24537 100%)",
-				backgroundImage:"-webkit-linear-gradient(top, #c62d1f 5%, #f24537 100%)",
-				backgroundImage:"-o-linear-gradient(top, #c62d1f 5%, #f24537 100%)",
-				backgroundImage:"-ms-linear-gradient(top, #c62d1f 5%, #f24537 100%)",
-				backgroundImage:"linear-gradient(to bottom, #c62d1f 5%, #f24537 100%)",
-				filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#c62d1f', endColorstr='#f24537',GradientType=0)"
-			});
-		},function(){
-			$(this).css({
-			backgroundImage:"-moz-box-shadow:inset 0px 1px 0px 0px #bd1a0b",
-			backgroundImage:"-webkit-box-shadow:inset 0px 1px 0px 0px #bd1a0b",
-			backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f24537), color-stop(1, #c62d1f))",
-			backgroundImage:"-moz-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-webkit-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-o-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"-ms-linear-gradient(top, #f24537 5%, #c62d1f 100%)",
-			backgroundImage:"linear-gradient(to bottom, #f24537 5%, #c62d1f 100%)",
-			filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#f24537', endColorstr='#c62d1f',GradientType=0)",
-			backgroundColor:"#f24537",
-			backgroundImage:"-moz-border-radius:32px",
-			backgroundImage:"-webkit-border-radius:32px"
-			});
-		});
-    }); 
-	//decisionDislikeButton.className = "ys-btn";
 	decisionDislikeButton.addEventListener("click", eatinderDisliked);
-	decisionDislikeButton.innerHTML = "BEĞENME";
+	decisionDislikeButton.innerHTML = "<i style='color:red;'class='fas fa-times fa-5x'></i>";
 	
 	var decisionLikeDiv = document.createElement('div');
 	decisionLikeDiv.style = "top: 77%; left:55%; width: 15%; height: 10%; position:fixed";
 	
-	var decisionLikeButton = document.createElement("button");
+	var decisionLikeButton = document.createElement("div");
 	decisionLikeButton.id = "eatinder-like-btn";
 	decisionLikeButton.addEventListener("click", eatinderLiked);
-	$(document).ready(function(){
-		$("#eatinder-like-btn").css({
-			borderRadius:"32px",
-			border:"3px solid #268a16",
-			display:"inline-block",
-			cursor:"pointer",
-			color:"#ffffff",
-			fontFamily:"Georgia",
-			fontSize:"16px",
-			fontWeight:"bold",
-			padding:"11px 37px",
-			textDecoration:"none",
-			textShadow:"0px -2px 0px #2f6627",
-			width:"200px",
-			backgroundImage:"-moz-box-shadow:inset 0px 1px 0px 0px #3dc21b",
-			backgroundImage:"-webkit-box-shadow:inset 0px 1px 0px 0px #3dc21b",
-			boxShadow:"inset 0px 1px 0px 0px #3dc21b",
-			backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cb811), color-stop(1, #77d42a))",
-			backgroundImage:"-moz-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-webkit-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-o-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-ms-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"linear-gradient(to bottom, #5cb811 5%, #77d42a 100%)",
-			filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cb811', endColorstr='#77d42a',GradientType=0)",
-			backgroundColor:"#5cb811",
-			backgroundImage:"-moz-border-radius:32px",
-			backgroundImage:"-webkit-border-radius:32px"
-	   });
-	   $("#eatinder-like-btn").hover(function(){
-			$(this).css({
-				backgroundColor:"#77d42a",
-				backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #77d42a), color-stop(1, #5cb811))",
-				backgroundImage:"-moz-linear-gradient(top, #77d42a 5%, #5cb811 100%)",
-				backgroundImage:"-webkit-linear-gradient(top, #77d42a 5%, #5cb811 100%)",
-				backgroundImage:"-o-linear-gradient(top, #77d42a 5%, #5cb811 100%)",
-				backgroundImage:"-ms-linear-gradient(top, #77d42a 5%, #5cb811 100%)",
-				backgroundImage:"linear-gradient(to bottom, #77d42a 5%, #5cb811 100%)",
-				filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#77d42a', endColorstr='#5cb811',GradientType=0)"
-			});
-		},function(){
-			$(this).css({
-			backgroundImage:"-moz-box-shadow:inset 0px 1px 0px 0px #3dc21b",
-			backgroundImage:"-webkit-box-shadow:inset 0px 1px 0px 0px #3dc21b",
-			backgroundImage:"-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cb811), color-stop(1, #77d42a))",
-			backgroundImage:"-moz-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-webkit-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-o-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"-ms-linear-gradient(top, #5cb811 5%, #77d42a 100%)",
-			backgroundImage:"linear-gradient(to bottom, #5cb811 5%, #77d42a 100%)",
-			filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cb811', endColorstr='#77d42a',GradientType=0)",
-			backgroundColor:"#5cb811",
-			backgroundImage:"-moz-border-radius:32px",
-			backgroundImage:"-webkit-border-radius:32px"
-			});
-		});
-   
-    }); 
-	//decisionLikeButton.className = "ys-btn";
-	decisionLikeButton.innerHTML = "BEĞEN";
+	decisionLikeButton.innerHTML = "<i style='color:green;' class='fas fa-heart fa-5x'></i>";
 	
 	decisionImgDiv.append(decisionImgImg);
 	decisionDislikeDiv.append(decisionDislikeButton);
 	decisionLikeDiv.append(decisionLikeButton);
+	decisionBackgroundImgDiv.append(decisionBackgroundImgImg);
+	decisionDiv.append(decisionBackgroundImgDiv);
 	decisionDiv.append(decisionImgDiv);
 	decisionDiv.append(decisionDislikeDiv);
 	decisionDiv.append(decisionLikeDiv);
@@ -265,7 +164,7 @@ function cantDecideButtonEvent() {
 	//});
 }
 
-function matchScreenAndResult() {
+function showMatchScreen() {
 	$('#eatinder-decision-div').fadeOut();
 
 	var matchDiv = document.createElement('div');
@@ -324,6 +223,12 @@ function onWindowLoad() {
 	if (!isRestaurantListingPage()) {
 		return;
 	}
+	
+	var cssLink = chrome.extension.getURL("css/all.css");
+	var jsLink = chrome.extension.getURL("js/all.js");
+	
+	$('head').append('<link rel="stylesheet" href="' + cssLink + '" type="text/css" />');
+	$('head').append('<script defer src="' + jsLink + '"></script>');
 	
 	var cantDecideButton = document.createElement("button");
 	cantDecideButton.innerHTML = "Karar Veremiyorum";
