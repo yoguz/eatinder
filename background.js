92b1460af38864a9c7c28f464f3bad6e235111e5
eOguzhan <e.oguzhan.yildirim@gmail.com>
2019-07-11 15:14:39 +0300

    sunum icin fotolar

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ var eatinderCuisinePictureMap = {
 	"Ev Yemekleri": "Ev_Yemekleri.jpg",
 	"Fast Food & Sandwich": "Fastfood_and_Sandwich.jpg",
 	"Kahvaltı": "Kahvalti.jpg",
-	"Kebap & Türk Mutfağı": "Kebap_and_Turk Mutfagi.jpg",
+	"Kebap & Türk Mutfağı": "Kebap_and_Turk_Mutfagi.jpg",
 	"Kumpir": "Kumpir.jpg",
 	"Köfte": "Kofte.jpg",
 	"Pasta & Tatlı": "Pasta_and_Tatli.jpeg",
@@ -88,8 +88,11 @@ function showResults() {
 	}
 	
 	console.log(finalRestaurantList);
-	var objTooltip = JSON.parse(finalRestaurantList[0].childNodes[1].childNodes[3].childNodes[1].dataset.tooltip);
-	console.log(objTooltip["ImageFullPath"]);
+	for (var i = 0; i < 3; ++i) {
+		var objTooltip = JSON.parse(finalRestaurantList[i].childNodes[1].childNodes[3].childNodes[1].dataset.tooltip);
+		console.log(objTooltip["ImageFullPath"]);
+	}
+	
 }
 
 // shows next food picture if any left, or shows match picture
@@ -97,8 +100,8 @@ function showNextUnswipedCuisine(){
 	if (eatinderSwipeCuisines.length > 0) {		// Show next food picture
 		var nextCuisine = eatinderSwipeCuisines.pop();
 		var cuisineJpg = eatinderCuisinePictureMap[nextCuisine];
-		var jpegImgElem = document.getElementById("eatinder-image-img");
-		jpegImgElem.src = chrome.extension.getURL("images/" + cuisineJpg);
+		var jpegImgElem = document.getElementById("eatinder-swipe");
+		jpegImgElem.src = chrome.extension.getURL("images2/" + cuisineJpg);
 		eatinderCurrentCuisine = nextCuisine;
 	} else {	// Show match picture
 		showMatchScreen();
@@ -111,53 +114,62 @@ function cantDecideButtonEvent() {
 	var cuisineCountArray = getAllCuisinesWithCounts();
 	fillSwipeCuisineList(cuisineCountArray);
 	
-	var decisionDiv = document.createElement('div');
-	decisionDiv.id = 'eatinder-decision-div';
-	decisionDiv.tabindex = "-1";
-	decisionDiv.style = "display: block; z-index: 1; visibility: visible; top: 0px; left: 0px; position: fixed; width: 100%; height: 100%;";
+	var eatinderDiv = document.createElement('div');
+	eatinderDiv.id = 'eatinder-div';
+	eatinderDiv.tabindex = "-1";
+	eatinderDiv.style = "display: block; z-index: 1; visibility: visible; top: 0px; left: 0px; position: fixed; width: 100%; height: 100%; background-color:rgba(185, 176, 176, 0.65);";
 	
-	var decisionBackgroundImgDiv = document.createElement("div");
-	decisionBackgroundImgDiv.id = 'eatinder-decision-background-img-div';
-	decisionBackgroundImgDiv.style= "top: 9%; left:28%; width: 44%; height: 90%; position:fixed; background-color:rgba(255, 255, 255, 1); border-radius:4px;";
+	var demoDiv = document.createElement('div');
+	demoDiv.className = "eatinder";
+	demoDiv.id = "eatinder-inner-div";
+	var headerDiv = document.createElement('div');
+	headerDiv.className = "eatinder__header";
+	var demoContentDiv = document.createElement('div');
+	demoContentDiv.className = "eatinder__content";
+	var demoButtonDiv = document.createElement('div');
+	demoButtonDiv.className = "eatinder__button";
+	var demoButtonLike = document.createElement('div');
+	demoButtonLike.innerHTML = "<i class='far fa-check-circle fa-5x'></i>";
+	var demoButtonDisLike = document.createElement('div');
+	demoButtonDisLike.innerHTML = "<i class='far fa-times-circle fa-5x'></i>";
+	var demoCardContentDiv = document.createElement('div');
+	demoCardContentDiv.className = "eatinder__card-cont";
+	var demoCardDiv = document.createElement('div');
+	demoCardDiv.className = "eatinder__card";
+	var demoCardTopDiv = document.createElement('div');
+	demoCardTopDiv.className = "eatinder__card__top brown";
+	var demoCardTopImg = document.createElement('img');
+	demoCardTopImg.src = chrome.extension.getURL("images/Balik_ve_Deniz_Urunleri2.jpg");
+	demoCardTopImg.style = "height: 100%;width: 100%; object-fit:contain";
+	demoCardTopImg.id = "eatinder-swipe";
+	var demoCardRejectDiv = document.createElement('div');
+	demoCardRejectDiv.className = "eatinder__card__choice m--reject";
+	var demoCardLikeDiv = document.createElement('div');
+	demoCardLikeDiv.className = "eatinder__card__choice m--like";
+	var demoCardDragDiv = document.createElement('div');
+	demoCardDragDiv.className = "eatinder__card__drag";
+	var demoContentLogoImg = document.createElement('img');
+	demoContentLogoImg.src = chrome.extension.getURL("images/eatinder_logo_128x128.png");
+	demoContentLogoImg.className = "eatinder__logo";
 	
-	var decisionBackgroundImgImg = document.createElement("img");
-	decisionBackgroundImgImg.id = 'eatinder-decision-background-img-img';
-	decisionBackgroundImgImg.src =  chrome.extension.getURL("images/Pattern_Food_1.jpg");
-	decisionBackgroundImgImg.style= "top: 10%; left:29%; width: 42%; height: 90%; position:fixed; opacity: 0.8";
 	
-	var decisionImgDiv = document.createElement('div');
-	decisionImgDiv.style = "top: 15%; left:30%; width: 40%; height: 60%; position:fixed";
+	demoDiv.append(headerDiv);
+	demoDiv.append(demoContentDiv);
+	demoDiv.append(demoButtonDiv);
+	demoContentDiv.append(demoCardContentDiv);
+	demoCardContentDiv.append(demoCardDiv);
+	demoCardDiv.append(demoCardTopDiv);
+	demoCardTopDiv.append(demoCardTopImg);
+	demoCardDiv.append(demoCardRejectDiv);
+	demoCardDiv.append(demoCardLikeDiv);
+	demoCardDiv.append(demoCardDragDiv);
+	demoButtonDiv.append(demoButtonLike);
+	demoButtonDiv.append(demoButtonDisLike);
+	headerDiv.append(demoContentLogoImg);
 	
-	var decisionImgImg = document.createElement("img");
-	decisionImgImg.id = 'eatinder-image-img';
-	decisionImgImg.style = "width: 100%; height: 100%;";
-	
-	var decisionDislikeDiv = document.createElement('div');
-	decisionDislikeDiv.style = "top: 77%; left:30%; width: 15%; height: 10%; position:fixed";
-	
-	var decisionDislikeButton = document.createElement("div");
-	decisionDislikeButton.id = "eatinder-dislike-btn";
-	decisionDislikeButton.addEventListener("click", eatinderDisliked);
-	decisionDislikeButton.innerHTML = "<i style='color:red;'class='fas fa-times fa-5x'></i>";
-	
-	var decisionLikeDiv = document.createElement('div');
-	decisionLikeDiv.style = "top: 77%; left:55%; width: 15%; height: 10%; position:fixed";
-	
-	var decisionLikeButton = document.createElement("div");
-	decisionLikeButton.id = "eatinder-like-btn";
-	decisionLikeButton.addEventListener("click", eatinderLiked);
-	decisionLikeButton.innerHTML = "<i style='color:green;' class='fas fa-heart fa-5x'></i>";
-	
-	decisionImgDiv.append(decisionImgImg);
-	decisionDislikeDiv.append(decisionDislikeButton);
-	decisionLikeDiv.append(decisionLikeButton);
-	decisionBackgroundImgDiv.append(decisionBackgroundImgImg);
-	decisionDiv.append(decisionBackgroundImgDiv);
-	decisionDiv.append(decisionImgDiv);
-	decisionDiv.append(decisionDislikeDiv);
-	decisionDiv.append(decisionLikeDiv);
-	
-	document.body.append(decisionDiv);
+	eatinderDiv.append(demoDiv);
+
+	document.body.append(eatinderDiv);
 	showNextUnswipedCuisine();
 	//$('#decisionDiv').load(chrome.extension.getURL("decision.html"), function(){
 	//	showNextUnswipedCuisine();
@@ -165,12 +177,8 @@ function cantDecideButtonEvent() {
 }
 
 function showMatchScreen() {
-	$('#eatinder-decision-div').fadeOut();
-
-	var matchDiv = document.createElement('div');
-	matchDiv.id = 'eatinder-match-div';
-	matchDiv.tabindex = "-1";
-	matchDiv.style = "display: block; z-index: 1; visibility: visible; top: 0px; left: 0px; position: fixed; width: 100%; height: 100%; background-color:rgba(185, 176, 176, 0.65);";
+	$('#eatinder-inner-div').fadeOut(2000);
+	var matchDiv = document.getElementById("eatinder-div");
 	
 	var matchImgDiv = document.createElement('div');
 	matchImgDiv.style = "top: 15%; left:30%; width: 40%; height: 60%; position:fixed";
@@ -178,14 +186,12 @@ function showMatchScreen() {
 	var matchImgImg = document.createElement("img");
 	matchImgImg.id = 'eatinder-imagematch-img';
 	matchImgImg.style = "width: 100%; height: 100%;";
+	matchImgImg.src =  chrome.extension.getURL("images/Its_a_match.png");
 	
 	matchImgDiv.append(matchImgImg);
 	matchDiv.append(matchImgDiv);
 	//document.body.append(matchDiv);
-	$(matchDiv).hide().appendTo(document.body).fadeIn(2000);
-
-	var jpegImgElem = document.getElementById("eatinder-imagematch-img");
-	jpegImgElem.src = chrome.extension.getURL("images/Its_a_match.png");
+	$(matchImgDiv).hide().appendTo(matchDiv).fadeIn(2000);
 }
 
 // Returns {cuisine, count} object list
@@ -224,11 +230,15 @@ function onWindowLoad() {
 		return;
 	}
 	
-	var cssLink = chrome.extension.getURL("css/all.css");
-	var jsLink = chrome.extension.getURL("js/all.js");
+	var allCssLink = chrome.extension.getURL("css/all.css");
+	var styleCssLink = chrome.extension.getURL("css/style.css");
+	var allJsLink = chrome.extension.getURL("js/all.js");
+	var scriptJsLink = chrome.extension.getURL("js/tinder_script.js");
 	
-	$('head').append('<link rel="stylesheet" href="' + cssLink + '" type="text/css" />');
-	$('head').append('<script defer src="' + jsLink + '"></script>');
+	$('head').append('<link rel="stylesheet" href="' + allCssLink + '" type="text/css" />');
+	$('head').append('<link rel="stylesheet" href="' + styleCssLink + '" type="text/css" />');
+	$('head').append('<script defer src="' + allJsLink + '"></script>');
+	// $('head').append('<script defer src="' + scriptJsLink + '"></script>');
 	
 	var cantDecideButton = document.createElement("button");
 	cantDecideButton.innerHTML = "Karar Veremiyorum";
@@ -242,3 +252,85 @@ function onWindowLoad() {
 }
 
 window.onload = onWindowLoad;
+
+$(document).ready(function() {
+
+  var animating = false;
+  var cardsCounter = 0;
+  var numOfCards = 2;
+  var decisionVal = 80;
+  var pullDeltaX = 0;
+  var deg = 0;
+  var $card, $cardReject, $cardLike;
+
+  function pullChange() {
+    animating = true;
+    deg = pullDeltaX / 10;
+    $card.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
+
+    var opacity = pullDeltaX / 100;
+    var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
+    var likeOpacity = (opacity <= 0) ? 0 : opacity;
+    $cardReject.css("opacity", rejectOpacity);
+    $cardLike.css("opacity", likeOpacity);
+  };
+
+  function release() {
+
+    if (pullDeltaX >= decisionVal) {
+      $card.addClass("to-right");
+	  eatinderLiked();
+    } else if (pullDeltaX <= -decisionVal) {
+		eatinderDisliked();
+      $card.addClass("to-left");
+    }
+
+    if (Math.abs(pullDeltaX) >= decisionVal) {
+      $card.addClass("inactive");
+
+      setTimeout(function() {
+        $card.addClass("below").removeClass("inactive to-left to-right");
+        cardsCounter++;
+        if (cardsCounter === numOfCards) {
+          cardsCounter = 0;
+          $(".eatinder__card").removeClass("below");
+        }
+      }, 300);
+    }
+
+    if (Math.abs(pullDeltaX) < decisionVal) {
+      $card.addClass("reset");
+    }
+
+    setTimeout(function() {
+      $card.attr("style", "").removeClass("reset")
+        .find(".eatinder__card__choice").attr("style", "");
+
+      pullDeltaX = 0;
+      animating = false;
+    }, 300);
+  };
+
+  $(document).on("mousedown touchstart", ".eatinder__card:not(.inactive)", function(e) {
+    if (animating) return;
+
+    $card = $(this);
+    $cardReject = $(".eatinder__card__choice.m--reject", $card);
+    $cardLike = $(".eatinder__card__choice.m--like", $card);
+    var startX =  e.pageX || e.originalEvent.touches[0].pageX;
+
+    $(document).on("mousemove touchmove", function(e) {
+      var x = e.pageX || e.originalEvent.touches[0].pageX;
+      pullDeltaX = (x - startX);
+      if (!pullDeltaX) return;
+      pullChange();
+    });
+
+    $(document).on("mouseup touchend", function() {
+      $(document).off("mousemove touchmove mouseup touchend");
+      if (!pullDeltaX) return; // prevents from rapid click events
+      release();
+    });
+  });
+
+});
